dfd3d9bd58ab120fff9781c6f08bfb27ea90ab40
samip kalyani <samipkalyani@gmail.com>
2023-10-15 15:52:16 -0700

    add_comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
+
+// A comment
 
 function App() {
   return (
